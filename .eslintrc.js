49e294d65947a33cc6b16dc1f9277b67b0401127
Romain Racamier-Lafon <romain.racamier@gmail.com>
2019-10-19 21:10:41 +0200

    upgrade stack

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+const rules = require('./.eslintrc.rules')
+
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true
+  },
+  extends: [
+    'standard'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaVersion: 2018
+  },
+  plugins: [
+    'html'
+  ],
+  rules
+}
